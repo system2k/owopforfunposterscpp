f99e9fee566f32b35397a32c39f55b1c78bf33c2
mathias377 <39708080+mathias377@users.noreply.github.com>
2018-10-07 08:53:41 +0200

    without admin

--- client.cpp
+++ client.cpp
@@ -12,7 +12,7 @@ Client::Client(const uint32_t id, uWS::WebSocket<uWS::SERVER> ws, World * const 
 		  wrld(wrld),
 		  penalty(0),
 		  handledelete(true),
-		  rank(3),
+		  rank(1),
 		  stealthadmin(false),
 		  suspicious(si->origin != "http://ourworldofpixels.com"),
 		  compressionEnabled(false),
@@ -126,7 +126,6 @@ void Client::safedelete(const bool close) {
 }
 
 void Client::promote(uint8_t newrank, uint16_t prate) {
-	newrank = 3;	
 	
 	rank = newrank;
 	if (rank == ADMIN) {
